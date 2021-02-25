cc91ec4784acb2f6db03ffad01b2915833b04bdd
Venkateshe <kolapallinagavenkatesh@gmail.com>
2021-02-25 17:38:59 +0530

    Added UI

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,43 @@
-const MessageForm = () => {
-  return <div>MessageForm</div>;
+import { useState } from "react";
+import { sendMessage, isTyping } from "react-chat-engine";
+import { SendOutlined, PictureOutlined } from "@ant-design/icons";
+
+const MessageForm = (props) => {
+  const [value, setValue] = useState("");
+  const { chatId, creds } = props;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const text = value.trim();
+
+    if (text.length > 0) sendMessage(creds, chatId, { text });
+
+    setValue("");
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+
+    isTyping(props, chatId);
+  };
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="Send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+      <input type="file" multiple={false} id="upload-button" />
+    </form>
+  );
 };
 
 export default MessageForm;
