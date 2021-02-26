33a8da2709ea3f9c76fbc04135d9ec9e1834966a
Venkateshe <kolapallinagavenkatesh@gmail.com>
2021-02-26 14:55:03 +0530

    Added UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { ChatEngine } from "react-chat-engine";
-import ChatFeed from "./components/ChatFeed";
 
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 import "./App.css";
 
+const projectID = "14a2eb6c-7d58-418b-b6df-00f9ab8179d5";
+
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="14a2eb6c-7d58-418b-b6df-00f9ab8179d5"
-      userName="Venkatesh"
-      userSecret="venky1234"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 };
+
+// infinite scroll, logout, more customizations...
 
 export default App;
